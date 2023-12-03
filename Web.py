c3c17371ce3bf9b4a5863d6e5055710779f5bea3
FauxWaffle <127357534+FauxWaffle@users.noreply.github.com>
2023-12-03 07:59:35 -0600

    Added new feature that shows completed tasks. They cannot be removed yet from completed.

--- Web.py
+++ Web.py
@@ -2,6 +2,7 @@ import streamlit as stream
 import functions
 
 todos = functions.get_todos()
+comp_items = functions.get_complete()
 
 
 def add_todo():
@@ -10,20 +11,33 @@ def add_todo():
     functions.write_todos(todos)
     stream.session_state["new_todo"] = ""
 
+
+def comp_todo():
+    comp_item = todos.pop(index)
+    comp_items.append(comp_item)
+    functions.write_todos(todos)
+    functions.write_complete(comp_items)
+
+
 stream.title("My To-Do App")
-stream.write("Enter tasks to track below:")
+stream.write("Active Tasks:")
+
+stream.text_input(label="Enter a todo:", placeholder="Add new item...",
+                  on_change=add_todo, key="new_todo")
 
 for index, todo in enumerate(todos):
     checkbox = stream.checkbox(todo, key=todo)
     if checkbox:
-        todos.pop(index)
-        functions.write_todos(todos)
+        comp_todo()
         del stream.session_state[todo]
         stream.rerun()  # this is to rerun the code for checkboxes.
 
+stream.subheader("Completed Tasks:")
 
-stream.text_input(label="Enter a todo:", placeholder="Add new item...",
-                  on_change=add_todo, key="new_todo")
+for comp_item in comp_items:
+    stream.write(comp_item)
+
+
 
 # stream.session_state   <-- this will print out at the bottom of the page the detailed info
 # to show what the variable is sending to the page.
--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 import os
 
 FILEPATH = "todo_file.txt"
-
+FILEPATH_2 = 'completed.txt'
 
 def get_todos(filepath=FILEPATH):
     if not os.path.exists(filepath):
@@ -14,4 +14,17 @@ def get_todos(filepath=FILEPATH):
 def write_todos(todos_arg, filepath=FILEPATH):  # <-- this function will behave as a procedure.
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
+
+
+def write_complete(comp_arg, filepath_2=FILEPATH_2):
+    with open(filepath_2, 'w') as file_local:
+        file_local.writelines(comp_arg)
+
+
+def get_complete(filepath=FILEPATH_2):
+    if not os.path.exists(filepath):
+        open(filepath, 'w').close()
+    with open(filepath, 'r') as file_local:
+        active_complete = file_local.readlines()
+    return active_complete
 
